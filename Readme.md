d1e2f3d4245a1579e64e1f10cec3d4ce65aa1a39
RoryPotter <29921813+RoryPotter@users.noreply.github.com>
2020-01-10 11:16:56 +0000

    Setup

---
+++ Readme.md
@@ -0,0 +1,4 @@
+ToDo App
+========
+
+Some practice!
---
+++ src/Main.elm
@@ -0,0 +1,53 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (Html, h1, text)
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    h1 [] [ text "Todo App" ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { view = view
+        , init = \_ -> init
+        , update = update
+        , subscriptions = always Sub.none
+        }
